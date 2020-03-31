d27b8c0419e0e1c1cc59fe137c577e9c9d00fe86
jake <jake@kripke.castle>
2020-03-31 15:33:24 -0400

    Updating tests

---
+++ e2e/test/login-form.spec.ts
@@ -0,0 +1,20 @@
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+
+describe('Username field', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  /*
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('rideshare-frontend app is running!');
+  });
+  */
+
+  it('Should ')
+});
+<app-login _ngcontent-jnh-c2="" _nghost-jnh-c3=""><a _ngcontent-jnh-c3="">Login</a><!----></app-login>
